8a6fbfe6da20ec5016dd614afdfe4680bb086fea
Hyosu Kim <khyosu0603@gmail.com>
2022-01-13 01:42:30 +0900

    id와pw 값이 서버로 안불러와짐.

--- app/src/public/js/home/LoginTest.js
+++ app/src/public/js/home/LoginTest.js
@@ -8,40 +8,15 @@ btn.addEventListener("click", login);
 
 function login() {
     const req = {
-      id: id.value,
-      pw: pw.value,
+      id : id.value,
+      pw : pw.value,
     };
 
     fetch("/login", {
         method: "POST",
         headers: {
-            "Content-Type" : "applicaton/json"
+            "Content-Type": "applicaton/json",
         },
-        body : JSON.stringify(req)
+        body: JSON.stringify(req),
     });
-}
-
-/* (btn).on('click', function(){
-    if((id).val() == "") {
-        (id).next('label').addClass('warning');
-        setTimeout(function() {
-            $('label').removeClass('warning');
-        },1500);
-    }
-    else if((pw).val() == "") {
-        (pw).next('label').addClass('warning');
-        setTimeout(function() {
-            $('label').removeClass('warning');
-        },1500);
-    }
-    else if((id).val() == "김효수" && (pw).val() == "201810458") {
-        (".login_form").css({"display" : "none"});
-        (".npoint").css({"display" : "flex"});
-        (".id-text").css({"color" : "#fff"});
-    const name = (id).val();
-        (".id-text").text(name);
-
-    } else {
-        alert("정보가 일치하지 않습니다.");
-    }
-}); */
+};
