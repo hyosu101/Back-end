aea2a0bfe7e094924d3eb2faef25059b17bce7d7
Hyosu Kim <khyosu0603@gmail.com>
2022-01-12 23:49:24 +0900

    돔을 이용하여 HTML 객체 저장

--- app/src/public/js/home/LoginTest.js
+++ app/src/public/js/home/LoginTest.js
@@ -1,4 +1,40 @@
 "use strict";
 
-console.log("hello");
-console.log("bye");
+const id = document.querySelector("#id"),
+    pw = document.querySelector("#pw"),
+    btn = document.querySelector("button");
+
+btn.addEventListener("click", login);
+
+function login() {
+    const req = {
+      id: id.value,
+      pw: pw.value,
+    };
+    
+}
+
+/* (btn).on('click', function(){
+    if((id).val() == "") {
+        (id).next('label').addClass('warning');
+        setTimeout(function() {
+            $('label').removeClass('warning');
+        },1500);
+    }
+    else if((pw).val() == "") {
+        (pw).next('label').addClass('warning');
+        setTimeout(function() {
+            $('label').removeClass('warning');
+        },1500);
+    }
+    else if((id).val() == "김효수" && (pw).val() == "201810458") {
+        (".login_form").css({"display" : "none"});
+        (".npoint").css({"display" : "flex"});
+        (".id-text").css({"color" : "#fff"});
+    const name = (id).val();
+        (".id-text").text(name);
+
+    } else {
+        alert("정보가 일치하지 않습니다.");
+    }
+}); */
