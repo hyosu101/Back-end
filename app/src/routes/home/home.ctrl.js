7081c7ac0f297720135f6438f9f342fd2d3d67ec
Hyosu Kim <khyosu0603@gmail.com>
2022-01-13 15:05:01 +0900

    User 모델 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,36 +1,20 @@
 "use strict";
 
-const UserStorage = require("../../models/UserStorage");
+const User = require("../../models/User");
 
 const output = {
     home: (req, res) => {
         res.render("home/index");
     },
-
     login: (req, res) => {
         res.render("home/login");
     },
 };
 
-
 const process = {
     login: (req, res) => {
-        const id = req.body.id,
-          pw = req.body.pw;
-
-        const users = UserStorage.getUsers("id", "pw",);
-
-        const responce = {};
-        if (users.id.includes(id)) {
-            const idx = users.id.indexOf(id);
-            if (users.pw[idx] === pw) {
-                responce.success = true;
-                return res.json(responce);
-            }
-        }
-
-        responce.success = false;
-        responce.msg = "로그인 실패"
+        const user = new User(req.body);
+        const responce = user.login();
         return res.json(responce);
     },
 };
