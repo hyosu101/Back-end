822cd113defa1ba706ce3672e12d99b3d76973ed
Hyosu Kim <khyosu0603@gmail.com>
2022-01-13 13:31:47 +0900

    백엔드에서 로그인 기능 구현

--- app/app.js
+++ app/app.js
@@ -14,7 +14,7 @@ app.set("views", "./src/views");
 app.set("view engine", "ejs");
 app.use(express.static(`${__dirname}/src/public`));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended : true }));
 
 app.use("/", home); // use -> 미들 웨어를 등록해주는 메서드.
 
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -4,15 +4,35 @@ const output = {
     home: (req, res) => {
         res.render("home/index");
     },
-    
+
     login: (req, res) => {
         res.render("home/login");
     },
 };
 
+const users = {
+    id: ["김효수", "이동하", "김채령", "이보람"],
+    pw: ["1234", "12345", "123456", "1234567"],
+};
+
 const process = {
     login: (req, res) => {
-        console.log(req.body);
+        const id = req.body.id,
+          pw = req.body.pw;
+
+        if (users.id.includes(id)) {
+            const idx = users.id.indexOf(id);
+            if (users.pw[idx] === pw) {
+                return res.json({
+                    success: true,
+                });
+            }
+        }
+
+        return res.json({
+            success: false,
+            msg: "로그인 실패",
+        });
     },
 };
 
